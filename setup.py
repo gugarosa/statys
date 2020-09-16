0d7e2c5397a8b1ebc9135e103d4d552961d2456f
Gustavo Rosa <gth.rosa@uol.com.br>
2020-09-16 09:28:20 -0300

    Adding remaining initial files.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='statys',
+      version='1.0.0',
+      description='Statistical Analyzers',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Gustavo Rosa',
+      author_email='gth.rosa@uol.com.br',
+      url='https://github.com/gugarosa/statys',
+      license='GPL-3.0',
+      install_requires=['coverage>=5.3',
+                        'matplotlib>=3.3.2',
+                        'pylint>=2.6.0',
+                        'pytest>=6.0.2'
+                        ],
+      extras_require={
+          'tests': ['coverage',
+                    'pytest',
+                    'pytest-pep8',
+                    ],
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages())
