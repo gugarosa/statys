cb68c7de7c47f3e605523740d67aaf9365bf7324
Gustavo Rosa <gth.rosa@uol.com.br>
2020-10-12 12:40:58 -0300

    Finishing up p-value and h-index plot.

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
-import statys.utils.plotter as p
 import statys.tests.wilcoxon as w
+import statys.utils.plotter as p
 from statys.core import Distribution
 
 # Defining input arguments
 x = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
 y = [0.07, 0.14, 0.72, 0.32, 0.59, 0.43]
-z = [0.17, 0.14, 0.72, 0.32, 0.19, 0.43]
+z = [2.17, 9.14, 999.72, 8.32, 7.19, 9.43]
 l = [0.1, 0.14, 0.72, 0.32, 0.19, 0.03]
 
 # Creating the distribution
-d = Distribution(x, y, z)
+d = Distribution(x, y, z, l)
 
 wilc = w.signed_rank(d)
 
-p.plot_similarity(wilc, labels=['a', 'b', 'c'])
+p.plot_p_value(wilc, labels=['a', 'b', 'c', 'd'])
+p.plot_significance_index(wilc, labels=['a', 'b', 'c', 'd'])
--- statys/utils/plotter.py
+++ statys/utils/plotter.py
@@ -1,37 +1,47 @@
-"""
+"""Plot-based functionalities to visualize the statistical tests.
 """
 
-import numpy as np
-import matplotlib.colors as clr
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def _create_labels(size=1):
-    """
+    """Creates a list of labels strings.
+
+    Args:
+        size (int): Amount of strings to be created.
+
+    Returns:
+        A list of stringed labels, e.g., arg0, arg1, ..., argn.
+
     """
 
-    #
+    # Creates a list of empty labels
     labels = []
 
-    #
+    # Iterates through every possible size
     for i in range(size):
-        #
+        # Appends the label string
         labels.append(f'arg{i}')
 
     return labels
 
 
-def plot_similarity(sim_dict, labels=None):
-    """
-    """
+def _prepare_plot(n_args, labels, title):
+    """Prepares the plot with common definitions.
 
-    # Calculates the number of arguments by solving: y = x^2 - x
-    n_args = int(np.roots([1, -1, -len(sim_dict)])[0])
+    Args:
+        n_args (int): Number of arguments.
+        labels (list): List of stringed labels.
+        title (str): Title to be displayed.
 
-    # Instantiates the similarity matrix
-    sims = np.zeros((n_args, n_args))
+    Returns:
+        The axis property from the plot.
+
+    """
 
     # Creates the figure and its axis
-    _, ax = plt.subplots(figsize=(7, 5))
+    _, ax = plt.subplots()
 
     # Checks if labels exists and number of supplied labels equals the number of arguments
     if labels and len(labels) == n_args:
@@ -48,16 +58,104 @@ def plot_similarity(sim_dict, labels=None):
     ax.set_xticklabels(labels)
     ax.set_yticks(np.arange(0, n_args, 1))
     ax.set_yticklabels(labels)
+    ax.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
 
-    # Sets title and defines the grid
-    ax.set_title('title')
-    ax.grid(which='minor', color='k')
+    # Defines grid properties
+    ax.set_title(title)
+    ax.set_xticks(np.arange(n_args + 1) - 0.5, minor=True)
+    ax.set_yticks(np.arange(n_args + 1) - 0.5, minor=True)
+    ax.grid(which='minor', color='w', linestyle='-', linewidth=3)
+    ax.tick_params(which='minor', bottom=False, left=False)
 
-    # Creates the color map
-    color_map = clr.LinearSegmentedColormap.from_list('ColorMap', [(1.000, 1.000, 1.000), (0.984, 0.501, 0.447)])
+    # Iterates through every spine
+    for _, spine in ax.spines.items():
+        # Removes the outline border
+        spine.set_visible(False)
 
-    # Adds the similarities to the plot
-    ax.imshow(sims, cmap=color_map, interpolation='none')
+    return ax
+
+
+def plot_p_value(p_dict, color_map='YlGn', labels=None, title=None):
+    """Plots a p-value grid according to statistical results.
+
+    Args:
+        p_dict (dict): Dictionary of significances and p-values.
+        color_map (str): A color map from matplotlib.
+        labels (list): List of stringed labels.
+        title (str): Title to be displayed.
+
+    """
+
+    # Calculates the number of arguments by solving: y = x^2 - x
+    n_args = int(np.roots([1, -1, -len(p_dict)])[0])
+
+    # Prepares the plot using common-based definitions
+    ax = _prepare_plot(n_args, labels, title)
+
+    # Instantiates the p-valued matrix
+    p = np.zeros((n_args, n_args))
+
+    # Iterates through every dictionary item
+    for k, v in p_dict.items():
+        # Gathers the positions from the arguments
+        args = k.replace('arg', '').split('-')
+
+        # Transforms the positions into integers
+        i, j = int(args[0]), int(args[1])
+
+        # Replaces its value
+        p[i][j] = 1 - v[1]
+
+    # Iterates through the p-valued matrix
+    for (i, j), z in np.ndenumerate(p):
+        # Applies the corresponding value to the position
+        ax.text(j, i, '{:0.2f}'.format(1 - z), ha='center', va='center')
+
+    # Adds the significances to the plot
+    ax.imshow(p, cmap=color_map)
+
+    # Displays the plot
+    plt.show()
+
+
+def plot_significance_index(sig_dict, color_map='YlGn', labels=None, title=None):
+    """Plots a significance index grid according to statistical results.
+
+    Args:
+        sig_dict (dict): Dictionary of significances and p-values.
+        color_map (str): A color map from matplotlib.
+        labels (list): List of stringed labels.
+        title (str): Title to be displayed.
+
+    """
+
+    # Calculates the number of arguments by solving: y = x^2 - x
+    n_args = int(np.roots([1, -1, -len(sig_dict)])[0])
+
+    # Prepares the plot using common-based definitions
+    ax = _prepare_plot(n_args, labels, title)
+
+    # Instantiates the significance matrix
+    sigs = np.zeros((n_args, n_args), dtype='int')
+
+    # Iterates through every dictionary item
+    for k, v in sig_dict.items():
+        # Gathers the positions from the arguments
+        args = k.replace('arg', '').split('-')
+
+        # Transforms the positions into integers
+        i, j = int(args[0]), int(args[1])
+
+        # Replaces its value
+        sigs[i][j] = v[0]
+
+    # Iterates through the significance matrix
+    for (i, j), z in np.ndenumerate(sigs):
+        # Applies the corresponding value to the position
+        ax.text(j, i, z, ha='center', va='center')
+
+    # Adds the significances to the plot
+    ax.imshow(sigs, cmap=color_map)
 
     # Displays the plot
     plt.show()
