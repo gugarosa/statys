b135c0b2ad9b6543c1b28fbf94e382d154bda8fa
Gustavo Rosa <gth.rosa@uol.com.br>
2020-10-12 11:34:58 -0300

    Improving plotter implementation.

--- run.py
+++ run.py
@@ -5,10 +5,12 @@ from statys.core import Distribution
 # Defining input arguments
 x = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
 y = [0.07, 0.14, 0.72, 0.32, 0.59, 0.43]
+z = [0.17, 0.14, 0.72, 0.32, 0.19, 0.43]
+l = [0.1, 0.14, 0.72, 0.32, 0.19, 0.03]
 
 # Creating the distribution
-d = Distribution(x, y)
+d = Distribution(x, y, z)
 
 wilc = w.signed_rank(d)
 
-p.plot_similarity(wilc)
+p.plot_similarity(wilc, labels=['a', 'b', 'c'])
--- statys/utils/plotter.py
+++ statys/utils/plotter.py
@@ -1,42 +1,63 @@
+"""
+"""
+
 import numpy as np
 import matplotlib.colors as clr
 import matplotlib.pyplot as plt
 
-
-def plot_similarity(dic):
+def _create_labels(size=1):
     """
     """
 
     #
-    size = len(dic)
+    labels = []
 
     #
-    m = np.zeros((size, size))
+    for i in range(size):
+        #
+        labels.append(f'arg{i}')
 
-    #
-    m[0][1] = 0.14
-    m[1][0] = 0.14
+    return labels
 
-    # Creates figure and axis
+
+def plot_similarity(sim_dict, labels=None):
+    """
+    """
+
+    # Calculates the number of arguments by solving: y = x^2 - x
+    n_args = int(np.roots([1, -1, -len(sim_dict)])[0])
+
+    # Instantiates the similarity matrix
+    sims = np.zeros((n_args, n_args))
+
+    # Creates the figure and its axis
     _, ax = plt.subplots(figsize=(7, 5))
 
-    # Defines `x` axis properties
-    ax.set_xticks(np.arange(0, 2, 1))
-    ax.set_xticklabels(['arg0', 'arg1'])
+    # Checks if labels exists and number of supplied labels equals the number of arguments
+    if labels and len(labels) == n_args:
+        # If yes, it is good to go
+        pass
 
-    # Defines `y` axis properties
-    ax.set_yticks(np.arange(0, 2, 1))
-    ax.set_yticklabels(['arg0', 'arg1'])
+    # If not
+    else:
+        # Re-creates the labels list
+        labels = _create_labels(n_args)
 
-    # Sets title and subtitle
+    # Defines axis properties
+    ax.set_xticks(np.arange(0, n_args, 1))
+    ax.set_xticklabels(labels)
+    ax.set_yticks(np.arange(0, n_args, 1))
+    ax.set_yticklabels(labels)
+
+    # Sets title and defines the grid
     ax.set_title('title')
-
-    # Creates the grid
     ax.grid(which='minor', color='k')
 
+    # Creates the color map
     color_map = clr.LinearSegmentedColormap.from_list('ColorMap', [(1.000, 1.000, 1.000), (0.984, 0.501, 0.447)])
-    ax.imshow(m, cmap=color_map, interpolation='none')
+
+    # Adds the similarities to the plot
+    ax.imshow(sims, cmap=color_map, interpolation='none')
 
     # Displays the plot
     plt.show()
-
