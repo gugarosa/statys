8f19cb28256d8aea994069abdadfb261366b6f02
Gustavo Rosa <gth.rosa@uol.com.br>
2020-10-16 09:29:08 -0300

    Bumping version for future release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='statys',
-      version='1.0.0',
+      version='1.0.1',
       description='Statistical Analyzers',
       long_description=long_description,
       long_description_content_type='text/markdown',
