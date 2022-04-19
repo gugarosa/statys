6cffd07b30a16216dc50d953458fb91106f134a3
Gustavo Rosa <gth.rosa@uol.com.br>
2022-04-19 18:31:09 -0300

    chore(statys): Bumps version for future release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="statys",
-    version="1.0.1",
+    version="1.0.2",
     description="Statistical Analyzers",
     long_description=long_description,
     long_description_content_type="text/markdown",
